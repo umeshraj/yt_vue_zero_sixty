a6c6bef602ed79f229c9b8c1be686028f0d20667
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-02 07:17:30 -0400

    Getting and showing data from rest api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,25 @@
 <template>
-  <h3>Home page</h3>
+  <div class="row"></div>
 </template>
+
+<script>
+import PostService from "../PostService";
+const postService = new PostService();
+export default {
+  name: "Home",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    postService
+      .getAllPosts()
+      .then((res) => {
+        this.posts = res.data;
+        console.log(this.posts);
+      })
+      .catch((err) => console.log(err));
+  },
+};
+</script>
