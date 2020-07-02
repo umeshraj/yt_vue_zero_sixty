b46ccd64bc93da6c8326f8aef8a3daf8e5f7cacc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-07-02 07:06:20 -0400

    Setting up routes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <h2>App</h2>
+  <div>
+    <Navbar />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
